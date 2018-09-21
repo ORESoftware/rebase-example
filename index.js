b7e30d1994f76b26b0e39af4b46e17e05416191a
Alexander Mills <alex@oresoftware.com>
2018-09-20 17:37:25 -0700

    second commit

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ console.log('0th line (master)');
 console.log('first line (master).');
 
 console.log('first line (feature).');
+
+console.log('first line (feature).');
+
+console.log('second line (feature).');
+
